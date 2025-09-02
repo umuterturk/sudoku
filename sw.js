a003ad4b7118f0b1001aeafb22d1dfe4c1612927
umut erturk <umut.erturk@udemy.com>
2025-09-02 13:02:12 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,68 @@
+// Service Worker for Sudoku Game
+const CACHE_NAME = 'sudoku-game-v1';
+const urlsToCache = [
+  '/sudoku/',
+  '/sudoku/index.html',
+  // Dynamic assets will be cached on-demand
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  console.log('🔧 Service Worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('📦 Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        if (response) {
+          console.log('📱 Serving from cache:', event.request.url);
+          return response;
+        }
+        
+        console.log('🌐 Fetching from network:', event.request.url);
+        return fetch(event.request).then((response) => {
+          // Don't cache non-successful responses
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  console.log('✨ Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('🗑️ Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
